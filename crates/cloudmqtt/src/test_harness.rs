19ab1643a0e9f8845ec7a38c24357ae185fb2416
Matthias Beyer <mail@beyermatthias.de>
2025-03-31 13:16:25 +0200

    fixup! Add a test dsl for simple test writing

--- crates/cloudmqtt/src/test_harness.rs
+++ crates/cloudmqtt/src/test_harness.rs
@@ -4,29 +4,70 @@
 //   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
-pub struct TestHarness {}
+use std::collections::HashMap;
+
+#[derive(Debug, Default)]
+pub struct TestHarness {
+    brokers: HashMap<String, Broker>,
+    clients: HashMap<String, Client>,
+}
 
 impl TestHarness {
-    pub fn new() -> Self {
-        Self {}
-    }
-
-    pub fn start_broker(&mut self, _name: String) -> Result<(), TestHarnessError> {
+    pub fn start_broker(&mut self, name: String) -> Result<(), TestHarnessError> {
+        self.brokers.insert(name, Broker::new());
         Ok(())
     }
 
-    pub fn create_client(&mut self, _name: String) -> Result<(), TestHarnessError> {
+    pub fn create_client(&mut self, name: String) -> Result<(), TestHarnessError> {
+        self.clients.insert(name, Client::new());
         Ok(())
     }
 
     pub fn connect_client_to_broker(
         &mut self,
-        _client_name: String,
-        _broker_name: String,
+        client_name: String,
+        broker_name: String,
     ) -> Result<(), TestHarnessError> {
-        Ok(())
+        let broker = self
+            .brokers
+            .get_mut(&broker_name)
+            .ok_or(TestHarnessError::BrokerNotFound(broker_name))?;
+        let client = self
+            .clients
+            .get_mut(&client_name)
+            .ok_or(TestHarnessError::ClientNotFound(client_name))?;
+
+        client.connect_to(broker)
+    }
+}
+
+#[derive(Debug, Default)]
+struct Broker {}
+
+impl Broker {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+#[derive(Debug, Default)]
+struct Client {}
+
+impl Client {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn connect_to(&mut self, _broker: &mut Broker) -> Result<(), TestHarnessError> {
+        todo!()
     }
 }
 
 #[derive(Debug, thiserror::Error)]
-pub enum TestHarnessError {}
+pub enum TestHarnessError {
+    #[error("Broker '{}' not found", .0)]
+    BrokerNotFound(String),
+
+    #[error("Client '{}' not found", .0)]
+    ClientNotFound(String),
+}
