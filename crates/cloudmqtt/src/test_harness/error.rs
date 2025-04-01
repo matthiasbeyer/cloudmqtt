ec2413897226b09ba8b7c99f7edb3c29a74d4ca0
Matthias Beyer <mail@beyermatthias.de>
2025-04-01 13:05:48 +0200

    fixup! Implement waiting for connect on broker verb

--- crates/cloudmqtt/src/test_harness/error.rs
+++ crates/cloudmqtt/src/test_harness/error.rs
@@ -26,7 +26,7 @@ pub enum TestHarnessError {
     StreamClosed(String),
 
     #[error("Received not expected packet: '{:?}'", .got)]
-    PacketNotExpected { got: crate::codec::MqttPacket },
+    PacketNotExpected { got: Box<crate::codec::MqttPacket> },
 
     #[error("Unexpected client identifier: got {}, expected {}", .got, .expected)]
     UnexpectedClientIdentifier { got: String, expected: String },
