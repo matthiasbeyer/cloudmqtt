39b6386b31b5255f068ec303decb51ea84f08238
Matthias Beyer <mail@beyermatthias.de>
2025-04-02 08:36:31 +0200

    fixup! Refactor condition checking implementations

--- crates/cloudmqtt/src/test_harness/error.rs
+++ crates/cloudmqtt/src/test_harness/error.rs
@@ -40,30 +40,3 @@ pub enum TestHarnessError {
     #[error("Packet payload is not valid UTF8")]
     PayloadNotUtf8,
 }
-
-/// Helper trait for error construction
-trait MqttPacketTypeName {
-    fn mqtt_packet_type_name(&self) -> &'static str;
-}
-
-impl MqttPacketTypeName for crate::codec::MqttPacket {
-    fn mqtt_packet_type_name(&self) -> &'static str {
-        match self.get_packet() {
-            mqtt_format::v5::packets::MqttPacket::Auth(_) => "Auth",
-            mqtt_format::v5::packets::MqttPacket::Connack(_) => "Connack",
-            mqtt_format::v5::packets::MqttPacket::Connect(_) => "Connect",
-            mqtt_format::v5::packets::MqttPacket::Disconnect(_) => "Disconnect",
-            mqtt_format::v5::packets::MqttPacket::Pingreq(_) => "Pingreq",
-            mqtt_format::v5::packets::MqttPacket::Pingresp(_) => "Pingresp",
-            mqtt_format::v5::packets::MqttPacket::Puback(_) => "Puback",
-            mqtt_format::v5::packets::MqttPacket::Pubcomp(_) => "Pubcomp",
-            mqtt_format::v5::packets::MqttPacket::Publish(_) => "Publish",
-            mqtt_format::v5::packets::MqttPacket::Pubrec(_) => "Pubrec",
-            mqtt_format::v5::packets::MqttPacket::Pubrel(_) => "Pubrel",
-            mqtt_format::v5::packets::MqttPacket::Suback(_) => "Suback",
-            mqtt_format::v5::packets::MqttPacket::Subscribe(_) => "Subscribe",
-            mqtt_format::v5::packets::MqttPacket::Unsuback(_) => "Unsuback",
-            mqtt_format::v5::packets::MqttPacket::Unsubscribe(_) => "Unsubscribe",
-        }
-    }
-}
